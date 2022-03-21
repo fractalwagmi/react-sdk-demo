40e595c75daa34272c65113b7f702df1197bdd93
Daniel Borstelmann <dborstelmann@gmail.com>
2022-03-21 09:20:55 -0600

    demo env

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-import Layout from 'components/main';
+import Head from 'next/head';
 
 const IndexPage = () => {
   const [userId, setUserId] = useState<string | undefined>();
+  const ref = useRef<HTMLIFrameElement>(null);
 
   useEffect(() => {
     window.addEventListener('message', event => {
-      if (event.data) {
+      if (event.data.event === 'loggedIn') {
         setUserId(event.data.userId);
+      }
+      if (event.data.event === 'loggedOut') {
+        setUserId(undefined);
       }
     });
   }, []);
@@ -26,19 +29,41 @@ const IndexPage = () => {
     });
   };
 
+  const doLogin = () => {
+    ref?.current?.contentWindow?.postMessage({ event: 'login' }, '*');
+  };
+  console.log();
+
   return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
-      <iframe width={700} height={700} src={'http://localhost:3000/iframe'} />
-      <div>userId</div>
+    <div>
+      <Head>
+        <title>SDK Demo Game</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <h1>SDK Demo Game ❄️</h1>
+      <div style={{ marginTop: '1rem' }}>
+        <button onClick={doLogin}>Login with Fractal</button>
+      </div>
+      <div style={{ marginTop: '1rem' }}>
+        <iframe
+          ref={ref}
+          frameBorder={0}
+          width={280}
+          height={40}
+          src={
+            window.location.origin.includes('http://localhost')
+              ? 'http://localhost:3000/iframe'
+              : 'https://fractal-git-wallet-auth.fractalpreview.com/iframe'
+          }
+        />
+      </div>
+      <div style={{ marginTop: '1rem' }}>userId</div>
       <div>{userId}</div>
-      {userId && <button onClick={registerKill}>Kill someone</button>}
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
-    </Layout>
+      <div style={{ marginTop: '1rem' }}>
+        {userId && <button onClick={registerKill}>Kill someone</button>}
+      </div>
+    </div>
   );
 };
 
