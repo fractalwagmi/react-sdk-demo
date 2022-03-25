09da786edd91196378bf8e41b6aa2aa8ee4d8238
Kevin King <4kevinking@gmail.com>
2022-03-24 17:36:02 -0700

    send bearer token, use test tournament id

--- pages/api/score/index.ts
+++ pages/api/score/index.ts
@@ -1,6 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
+  if (!process.env.FRACTAL_AUTH_TOKEN) {
+    // eslint-disable-next-line no-console
+    console.error('FRACTAL_AUTH_TOKEN must be set');
+  }
   try {
     const response = await fetch(
       'https://fractal-core-grpc-server-rest-2-tmg6xh47ja-uc.a.run.app/v1/scores',
@@ -9,8 +13,10 @@ const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
           projectId: '5701666712059904',
           scoreType: 'Kills',
           scoreValue: 1,
+          tournamentId: 'test_one',
           userId: _req.body.userId,
         }),
+        headers: { authorization: `Bearer ${process.env.FRACTAL_AUTH_TOKEN}` },
         method: 'POST',
       },
     );
