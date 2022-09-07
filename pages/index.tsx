fe6682730625c6c6f717b45cb8a0e6a0ac2ec772
Alex Wormuth <alex.wormuth@gmail.com>
2022-09-07 11:58:49 -0500

    use new react-sdk package and clean up ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { FractalProvider } from '@fractalwagmi/fractal-sdk';
+import { FractalProvider } from '@fractalwagmi/react-sdk';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 
@@ -15,11 +15,11 @@ const IndexPage = () => {
   return (
     <div>
       <Head>
-        <title>SDK Demo Game</title>
+        <title>React SDK Demo</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <h1>SDK Demo Game ❄️</h1>
+      <h1>React SDK Demo ❄️</h1>
       <div>
         <FractalProvider clientId={CLIENT_ID}>
           <FractalWallet />
